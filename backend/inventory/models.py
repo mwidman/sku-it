4ddbf54b17ead789464db329b7810566d3a21165
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-09 22:23:23 -0700

    Added default value to current_quantity field.

--- backend/inventory/models.py
+++ backend/inventory/models.py
@@ -6,15 +6,16 @@ from django.core.validators import MinValueValidator
 from django.utils import timezone
 
 
-
 class ChoiceEnum(Enum):
     @classmethod
     def choices(cls):
         return tuple((x.name, x.value) for x in cls)
 
+
 class TxType(ChoiceEnum):
     PURCHASED = 'purchased'
     SOLD = 'sold'
+
 
 '''
 class Units(models.Model):
@@ -35,7 +36,7 @@ class Sku(models.Model):
     #base_units = models.OneToOneField(Units, on_delete=models.SET_NULL, null=True)
     base_units = models.CharField(max_length=24)
     current_quantity = models.DecimalField(max_digits=16, decimal_places=2,
-        validators=[MinValueValidator(Decimal('0.00'))])
+                                           validators=[MinValueValidator(Decimal('0.00'))], default=Decimal('0.00'))
 
 
 class Transaction(models.Model):
@@ -43,8 +44,8 @@ class Transaction(models.Model):
     tx_type = models.CharField(
         max_length=20, choices=TxType.choices(), default=TxType.PURCHASED)
     quantity = models.DecimalField(max_digits=16, decimal_places=2,
-        validators=[MinValueValidator(Decimal('0.01'))], null=False)
+                                   validators=[MinValueValidator(Decimal('0.01'))], null=False)
     total_cost = models.DecimalField(max_digits=16, decimal_places=2,
-        validators=[MinValueValidator(Decimal('0.01'))], default=Decimal('0.00'))
+                                     validators=[MinValueValidator(Decimal('0.01'))], default=Decimal('0.00'))
     client = models.CharField(max_length=200, null=False)
     tx_date = models.DateTimeField(default=timezone.now)
