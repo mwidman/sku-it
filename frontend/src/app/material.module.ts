45abeb0e3b0237a8eb8e3cb0afc1c4bf264a53b2
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-11 09:48:00 -0700

    Some additional styling for the dashboard.

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,17 +1,29 @@
 import { NgModule } from '@angular/core'; 
 import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
   MatButtonModule,
   MatCardModule,
+  MatIconModule,
 } from '@angular/material';
 
 @NgModule({
   imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
   ],
   exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
     MatButtonModule,
     MatCardModule,
+    MatIconModule,
   ]
 })
 export class MaterialModule {}
