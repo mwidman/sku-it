6cd255e7c1f6798e44fbbba1d2a168384c0a3920
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-11 14:55:23 -0700

    Added validation to prevent negative sku quantities.

--- frontend/src/app/inventory/components/transaction-add/transaction-add.component.ts
+++ frontend/src/app/inventory/components/transaction-add/transaction-add.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 import * as fromInventory from '../../reducers';
 import { TxType } from '../../models/transaction';
@@ -14,17 +15,17 @@ import { AddTransaction } from '../../actions/transaction.actions';
   templateUrl: './transaction-add.component.html',
   styleUrls: ['./transaction-add.component.scss']
 })
-export class TransactionAddComponent implements OnInit {
+export class TransactionAddComponent implements OnInit, OnDestroy {
 
   txForm: FormGroup = new FormGroup({
     client: new FormControl('', [Validators.required]),
-    quantity: new FormControl('', [Validators.required, Validators.min(0)]),
+    //quantity: new FormControl('', [Validators.required, Validators.min(0)]),
+    quantity: new FormControl('', [Validators.required, Validators.min(0)], (ctrl) => this.checkSkuQuantity(ctrl)),
     total_cost: new FormControl('', [Validators.min(0)]),
   });
-
   txType: string;
-
   sku$: Observable<Sku>;
+  private formSub: Subscription;
 
   constructor(
     private store: Store<fromInventory.State>,
@@ -36,6 +37,10 @@ export class TransactionAddComponent implements OnInit {
     this.sku$ = this.store.pipe(
       select(fromInventory.getSelectedSku)
     );
+  }
+
+  ngOnDestroy() {
+
   }
 
   onSubmit() {
@@ -50,11 +55,27 @@ export class TransactionAddComponent implements OnInit {
     return this.txForm.controls[controlName].hasError(errorName);
   }
 
-  convertTxType() {
+  convertTxType(): string {
     switch(this.txType) {
       case TxType.SOLD: return 'Shipping';
       case TxType.PURCHASED: return 'Receiving';
     }
+  }
+
+  checkSkuQuantity(ctrl: AbstractControl): Observable<null|ValidationErrors> {
+    return this.sku$.pipe(
+      take(1),
+      map((sku) => {
+        if(this.txType == TxType.SOLD &&
+          (sku.current_quantity - ctrl.value <= 0)) {
+          return { negativeQuantity: true };
+        }
+        else {
+          return null;
+        }
+      })
+    )
+
   }
 
 }
