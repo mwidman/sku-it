c33f312bded6cc8c92573a7561598125b0985203
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-12 12:22:10 -0700

    Fixing creation of Transaction records and validating that they don't cause negative inventory.

--- backend/inventory/serializers.py
+++ backend/inventory/serializers.py
@@ -3,13 +3,31 @@ from rest_framework import serializers
 from inventory.models import Sku, Transaction
 
 
-class SkuSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Sku
-        fields = ('name', 'description', 'base_units', 'current_quantity')
-
-
 class TransactionSerializer(serializers.HyperlinkedModelSerializer):
+
+    sku = serializers.PrimaryKeyRelatedField(queryset=Sku.objects.all())
+
     class Meta:
         model = Transaction
-        fields = ('tx_type', 'quantity', 'total_cost', 'tx_date')
+        resource_name='transactions'
+        fields = ('id', 'sku', 'tx_type', 'quantity', 'client', 'total_cost', 'tx_date')
+    
+    def create(self, validated_data):
+        print('validated_data = %s' % validated_data)
+        print('sku = %s' % validated_data['sku'])
+        obj = Transaction.objects.create(**validated_data)
+        obj.save()
+        return obj
+
+    def validate(self, attrs):
+        obj = Transaction(**attrs)
+        obj.clean()
+        return attrs
+
+class SkuSerializer(serializers.HyperlinkedModelSerializer):
+    transactions = TransactionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Sku
+        resource_name = 'skus'
+        fields = ('id', 'name', 'description', 'base_units', 'current_quantity', 'transactions')
