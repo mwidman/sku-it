c45caafa3227c2987ce7617745cb41c917fb0945
mwidman <2118673+mwidman@users.noreply.github.com>
2019-06-14 10:53:05 -0700

    Modifying service specs to allow them to pass.

--- frontend/src/app/inventory/services/sku.service.spec.ts
+++ frontend/src/app/inventory/services/sku.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 import { SkuService } from './sku.service';
 
 describe('SkuService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ 
+      { provide: 'BASE_API_URL', useValue: '/api' },
+      { provide: HttpClient, useClass: HttpTestingController },
+    ]
+  }));
 
   it('should be created', () => {
     const service: SkuService = TestBed.get(SkuService);
     expect(service).toBeTruthy();
   });
+
 });
--- frontend/src/app/inventory/services/transaction.service.spec.ts
+++ frontend/src/app/inventory/services/transaction.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
 
-import { TransactionService } from './sku.service';
+import { TransactionService } from './transaction.service';
 
 describe('TransactionService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ 
+      { provide: 'BASE_API_URL', useValue: '/api' },
+      { provide: HttpClient, useClass: HttpTestingController },
+    ]
+  }));
 
   it('should be created', () => {
     const service: TransactionService = TestBed.get(TransactionService);
     expect(service).toBeTruthy();
   });
+
 });
